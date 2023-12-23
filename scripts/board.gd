591e63542eb49bb08647150b1bfdcc83368a75a3
Jacpowerz <136922755+Jacpowerz@users.noreply.github.com>
2023-12-22 23:07:47 -0600

    Initialized piece types and added method to move pieces

--- scripts/board.gd
+++ scripts/board.gd
@@ -1,25 +1,34 @@
 extends Node2D
 
+# Set tile manager equal to the Tiles node in scene tree
+@onready var square_manager = $Squares
+@onready var piece_manager = $Pieces
 # Side length of board equal to viewport/window height
 var window_side_length = 600.0
-# Set tile manager equal to the Tiles node in scene tree
-@onready var tile_manager = $Tiles
 
-class Tile:
+class Square:
 	
 	extends ColorRect
 	
 	var body = ColorRect.new()
 	var side_length = 20
+	var center = Vector2.ZERO
 	
 	func _init(side_len = side_length):
 		size = Vector2(side_len, side_len)
 		
+class Piece:
 	
-	func _to_string():
-		return "Tile: " + self.name + " with position: " + str(self.position)
+	extends Sprite2D
+	
+	var side = ""
+	var current_square = Square.new()
+	
+	func move(target_square: Square):
+		current_square = target_square
+		position = target_square.center
 
-func make_board():
+func make_board(dark_color, light_color):
 	var column_indices = "abcdefgh".split()
 	var row_indices = "12345678".split()
 	var rows = 8
@@ -29,25 +38,60 @@ func make_board():
 	# Add tiles
 	for row in range(rows):
 		for column in range(columns):
-			var tile = Tile.new(tile_side_len)
+			var square = Square.new(tile_side_len)
 			
-			tile.position = Vector2 (
+			square.position = Vector2 (
 				column*tile_side_len,
 				row*tile_side_len
 			)
 			
+			square.center = square.position+Vector2(36.5,36.5)
+			
 			# Set alternating colors for light and dark squares
 			if ((row+column) % 2 == 0):
-				tile.color = Color.RED
+				square.color = dark_color
 			else:
-				tile.color = Color.BLUE
+				square.color = light_color
 			
 			# Set tile name to how it is in real life
-			tile.name = column_indices[column] + row_indices[row]
+			square.name = column_indices[column] + row_indices[row]
 			
 			# Add tiles to Tiles node
-			tile_manager.add_child(tile)
-			
+			square_manager.add_child(square)
+
+func initialize_pieces():
+	var base_path = "res://assets/tiles/64_"
+	for i in range(1,13):
+		
+		# Change filepath based on index
+		var piece_path = base_path + str(i) if (i >= 10) else base_path + "0" + str(i)
+		var piece_image = load(piece_path+".png")
+		
+		var piece = Piece.new()
+		piece.texture = piece_image
+		piece.scale = Vector2(1.25,1.25)
+		
+		# Assign pieces to side (black or white)
+		piece.side = "White" if (i <= 6) else "Black"
+		
+		match i:
+			1: piece.name = "R"
+			2: piece.name = "N"
+			3: piece.name = "B"
+			4: piece.name = "Q"
+			5: piece.name = "K"
+			6: piece.name = "P"
+			7: piece.name = "r"
+			8: piece.name = "n"
+			9: piece.name = "b"
+			10: piece.name = "q"
+			11: piece.name = "k"
+			12: piece.name = "p"
+		
+		piece.move($Squares/g8)
+		piece_manager.add_child(piece)
 
 func _ready():
-	make_board()
+	make_board(Color.WHEAT, Color.SADDLE_BROWN)
+	initialize_pieces()
+	print(piece_manager.get_child_count())
